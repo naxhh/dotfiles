aa676fd515a6b971ad6f2f32d0f45c7630664cc0
Ignacio Tolstoy <ignacio.tolstoy@schibsted.com>
2019-05-25 19:22:52 +0200

    Remove uneeded plugins

--- config.js
+++ config.js
@@ -13,15 +13,10 @@ module.exports = {
     'dos2unix',
     // Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
     'findutils',
-    // 'fortune',
     'fzf',
     'readline', // ensure gawk gets good readline
     'gawk',
-    // http://www.lcdf.org/gifsicle/ (because I'm a gif junky)
-    'gifsicle',
     'gnupg',
-    // Install GNU `sed`, overwriting the built-in `sed`
-    // so we can do "sed -i 's/foo/bar/' file" instead of "sed -i '' 's/foo/bar/' file"
     'gnu-sed --with-default-names',
     // upgrade grep so we can get things like inverted match (-v)
     'grep --with-default-names',
@@ -29,7 +24,6 @@ module.exports = {
     'homebrew/dupes/grep',
     // https://github.com/jkbrzt/httpie
     'httpie',
-    // jq is a sort of JSON grep
     'jq',
     // Mac App Store CLI: https://github.com/mas-cli/mas
     'mas',
@@ -51,47 +45,26 @@ module.exports = {
     'wget --enable-iri'
   ],
   cask: [
-    //'adium',
-    //'amazon-cloud-drive',
-    //'atom',
-    // 'box-sync',
-    //'comicbooklover',
-    //'diffmerge',
-    'docker', // docker for mac
-    //'dropbox',
-    //'evernote',
+    'docker',
     'flux',
     'gpg-suite',
-    //'ireadfast',
     'iterm2',
     'little-snitch',
     'macbreakz',
     'micro-snitch',
     'signal',
-    //'macvim',
     'sizeup',
-    //'sketchup',
     'slack',
     'the-unarchiver',
-    //'torbrowser',
-    //'transmission',
-    'visual-studio-code',
-    //'vlc',
+    'vlc',
     'xquartz'
   ],
   gem: [
   ],
   npm: [
-    'antic',
-    'buzzphrase',
     'eslint',
     'instant-markdown-d',
-    // 'generator-dockerize',
-    // 'gulp',
     'npm-check-updates',
-    'prettyjson',
-    'trash',
     'vtop'
-    // ,'yo'
   ]
 };
